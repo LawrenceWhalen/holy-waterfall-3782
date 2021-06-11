9a53e60614bbb62c75d025b0b9cf0229125f3afd
Lawrence Whalen <ldwhalen@gmail.com>
2021-06-11 08:54:44 -0600

    Hit save on flight_spec this time

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe Flight, type: :model do
     it { should have_many(:passengers).through(:flight_passengers) }
   end
 
+  describe 'instance methods' do
+    describe '.airline_name' do
+      it 'returns the name of that flights airline' do
+        airline_1= Airline.create!(name: 'Terminal')
+        flight_1 = airline_1.flights.create!(number: '1999', date: '08/06/2021', departure_city: 'Denver', arrival_city: 'Tokyo')
+
+        expect(flight_1.airline_name).to eq('Terminal')
+      end
+    end
+  end
 end
