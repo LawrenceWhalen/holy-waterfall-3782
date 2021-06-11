1caefd76cd110dc3723adbd222a6843c487cd2f1
Lawrence Whalen <ldwhalen@gmail.com>
2021-06-11 08:40:03 -0600

    Red airline name flights index

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe 'flights index' do
   before :each do
     @airline_1= Airline.create!(name: 'Terminal')
+    @airline_2= Airline.create!(name: 'May Day')
     @flight_1 = @airline_1.flights.create!(number: '1999', date: '08/06/2021', departure_city: 'Denver', arrival_city: 'Tokyo')
     @flight_2 = @airline_1.flights.create!(number: '1998', date: '08/05/2021', departure_city: 'Denver', arrival_city: 'Toronto')
-    @flight_3 = @airline_1.flights.create!(number: '2000', date: '09/06/2021', departure_city: 'Toronto', arrival_city: 'Tokyo')
+    @flight_3 = @airline_2.flights.create!(number: '2000', date: '09/06/2021', departure_city: 'Toronto', arrival_city: 'Tokyo')
 
     visit '/flights'
   end
@@ -20,6 +21,18 @@ RSpec.describe 'flights index' do
       expect(page).to have_content(@flight_1.number)
       expect(page).to have_content(@flight_2.number)
       expect(page).to have_content(@flight_3.number)
+    end
+
+    it 'displays airline names next to flights' do
+      within("##{@flight_1.id}") do
+        expect(page).to have_content(@airline_1.name)
+      end
+      within("##{@flight_2.id}") do
+        expect(page).to have_content(@airline_1.name)
+      end
+      within("##{@flight_3.id}") do
+        expect(page).to have_content(@airline_2.name)
+      end
     end
   end
 end
