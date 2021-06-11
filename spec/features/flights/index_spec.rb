cabf2ed1d3c079c8e27a282089f8c7d15330fdc7
Lawrence Whalen <ldwhalen@gmail.com>
2021-06-11 09:03:08 -0600

    Green passenger names flight index

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -7,6 +7,16 @@ RSpec.describe 'flights index' do
     @flight_1 = @airline_1.flights.create!(number: '1999', date: '08/06/2021', departure_city: 'Denver', arrival_city: 'Tokyo')
     @flight_2 = @airline_1.flights.create!(number: '1998', date: '08/05/2021', departure_city: 'Denver', arrival_city: 'Toronto')
     @flight_3 = @airline_2.flights.create!(number: '2000', date: '09/06/2021', departure_city: 'Toronto', arrival_city: 'Tokyo')
+    @passenger_1 = Passenger.create!(name: 'Alice', age: 34)
+    @passenger_2 = Passenger.create!(name: 'Bob', age: 45)
+    @passenger_3 = Passenger.create!(name: 'Judice', age: 67)
+    @passenger_4 = Passenger.create!(name: 'Wanda', age: 12)
+    FlightPassenger.create!(flight: @flight_1, passenger: @passenger_1)
+    FlightPassenger.create!(flight: @flight_2, passenger: @passenger_2)
+    FlightPassenger.create!(flight: @flight_3, passenger: @passenger_3)
+    FlightPassenger.create!(flight: @flight_1, passenger: @passenger_4)
+    FlightPassenger.create!(flight: @flight_2, passenger: @passenger_1)
+    FlightPassenger.create!(flight: @flight_3, passenger: @passenger_4)
 
     visit '/flights'
   end
@@ -32,6 +42,21 @@ RSpec.describe 'flights index' do
       end
       within("##{@flight_3.id}") do
         expect(page).to have_content(@airline_2.name)
+      end
+    end
+
+    it 'displays all passenger names for that flight' do
+      within("##{@flight_1.id}") do
+        expect(page).to have_content(@passenger_1.name)
+        expect(page).to have_content(@passenger_4.name)
+      end
+      within("##{@flight_2.id}") do
+        expect(page).to have_content(@passenger_2.name)
+        expect(page).to have_content(@passenger_1.name)
+      end
+      within("##{@flight_3.id}") do
+        expect(page).to have_content(@passenger_3.name)
+        expect(page).to have_content(@passenger_4.name)
       end
     end
   end
